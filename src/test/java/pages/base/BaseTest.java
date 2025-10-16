45d57570169c3bac6767902102bf997679ea29b5
Yogesh Gheu <yogeshgheu@MacBook-Air.local>
2025-10-16 21:15:33 +0530

    debug-5

--- src/test/java/pages/base/BaseTest.java
+++ src/test/java/pages/base/BaseTest.java
@@ -17,6 +17,9 @@ public class BaseTest {
         String baseUrl = PropertiesUtility.get("BASE_URL");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
         WebDriver driverInstance = new ChromeDriver(options);
