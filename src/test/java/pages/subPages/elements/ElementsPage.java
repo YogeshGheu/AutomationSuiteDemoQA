f5523263dcbc615e431a029002bb412645e17c03
yogeshgheu <yogeshgheu@gmail.com>
2025-10-26 17:44:25 +0530

    Add : Radio Button Tests

--- src/test/java/pages/subPages/elements/ElementsPage.java
+++ src/test/java/pages/subPages/elements/ElementsPage.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.base.BasePage;
 import pages.subPages.elements.childPages.CheckBoxPage;
+import pages.subPages.elements.childPages.RadioButtonPage;
 import pages.subPages.elements.childPages.TextBoxPage;
 
 public class ElementsPage extends BasePage {
 
     private final By textBoxLabelFromDropdown = By.xpath("//li/descendant::span[text()=\"Text Box\"]");
     private final By checkBoxLabelFromDropdown = By.xpath("//li/descendant::span[text()=\"Check Box\"]");
+    private final By radioButtonLabelFromDropdown = By.xpath("//li/descendant::span[text()=\"Radio Button\"]");
 
     public ElementsPage(WebDriver driver){
         super(driver);
@@ -24,5 +26,10 @@ public class ElementsPage extends BasePage {
     public CheckBoxPage clickCheckBoxLabelFromDropdown(){
         click(checkBoxLabelFromDropdown);
         return new CheckBoxPage(driver);
+    }
+
+    public RadioButtonPage clickRadioButtonLabelFromDropdown(){
+        click(radioButtonLabelFromDropdown);
+        return new RadioButtonPage(driver);
     }
 }
--- src/test/java/pages/subPages/elements/childPages/RadioButtonPage.java
+++ src/test/java/pages/subPages/elements/childPages/RadioButtonPage.java
@@ -1,4 +1,45 @@
 package pages.subPages.elements.childPages;
 
-public class RadioButtonPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import pages.base.BasePage;
+
+public class RadioButtonPage extends BasePage {
+
+    // radio buttons
+//    private final By yesRadio = By.xpath("//label[@for=\"yesRadio\"]");
+    private final By yesRadio = By.id("yesRadio");
+//    private final By impressiveRadio = By.xpath("//label[@for=\"impressiveRadio\"]");
+    private final By impressiveRadio = By.id("impressiveRadio");
+    private final By noRadio = By.id("noRadio");
+
+    // message
+    private final By outputText = By.xpath("//p[text()=\"You have selected \"]/child::span");
+
+    // constructor
+    public RadioButtonPage(WebDriver driver){
+        super(driver);
+    }
+
+    // methods
+    public boolean clickYesRadio(){
+        click(yesRadio);
+        return find(yesRadio).isSelected();
+    }
+
+    public boolean clickImpressiveRadio(){
+        click(impressiveRadio);
+        return find(impressiveRadio).isSelected();
+    }
+
+    public String getOutputText(){
+        return find(outputText).getText();
+    }
+
+    public boolean isNoRadioClickable(){
+        return find(noRadio).isEnabled();
+    }
+
 }
