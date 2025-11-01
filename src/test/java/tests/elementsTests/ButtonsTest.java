cfa572f909ca51307429babb6cbabd6236ebe0fc
yogeshgheu <yogeshgheu@gmail.com>
2025-11-01 18:44:36 +0530

    single click on element to focus on it before clicking the double click

--- src/test/java/tests/elementsTests/ButtonsTest.java
+++ src/test/java/tests/elementsTests/ButtonsTest.java
@@ -7,7 +7,7 @@ import pages.homepage.Homepage;
 import pages.subPages.elements.childPages.ButtonsPage;
 
 public class ButtonsTest extends BaseTest {
-    @Test(description = "test the buttons functionality")
+    @Test(description = "test the buttons functionality", invocationCount = 10)
     public void testButtons(){
         Homepage homepage = new Homepage(getDriver());
         ButtonsPage buttonsPage = homepage.clickElementsTile().clickButtonsLabelFromDropdown();
--- src/test/java/utilities/ActionsUtility.java
+++ src/test/java/utilities/ActionsUtility.java
@@ -18,7 +18,7 @@ public class ActionsUtility extends BaseUtility{
     }
 
     public static void doubleClick(WebElement element){
-        getActions().moveToElement(element).doubleClick(element).perform();
+        getActions().moveToElement(element).click().doubleClick(element).perform();
     }
 
     public static void rightClick(WebElement element){
