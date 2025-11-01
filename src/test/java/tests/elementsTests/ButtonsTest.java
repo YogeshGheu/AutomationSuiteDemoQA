c9510b4caf21b4d0f9789e1862e69e38072d7984
yogeshgheu <yogeshgheu@gmail.com>
2025-11-01 18:46:47 +0530

    remove invocationCount

--- src/test/java/tests/elementsTests/ButtonsTest.java
+++ src/test/java/tests/elementsTests/ButtonsTest.java
@@ -7,7 +7,7 @@ import pages.homepage.Homepage;
 import pages.subPages.elements.childPages.ButtonsPage;
 
 public class ButtonsTest extends BaseTest {
-    @Test(description = "test the buttons functionality", invocationCount = 10)
+    @Test(description = "test the buttons functionality")
     public void testButtons(){
         Homepage homepage = new Homepage(getDriver());
         ButtonsPage buttonsPage = homepage.clickElementsTile().clickButtonsLabelFromDropdown();
